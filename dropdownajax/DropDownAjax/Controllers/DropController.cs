59364209faae4896e6b6542abe8c05571bab99fe
shiva-rogx <65455314+shiva-rogx@users.noreply.github.com>
2021-07-13 23:18:37 +0530

    Drop Down Ajax

---
+++ dropdownajax/DropDownAjax/Controllers/DropController.cs
@@ -0,0 +1,30 @@
+﻿using DropDownAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropDownAjax.Controllers
+{
+    public class DropController : Controller
+    {
+        DropdownEntities db = new DropdownEntities();
+        public ActionResult Index()
+        {
+            List<Country> CountryList = db.Countries.ToList();
+            ViewBag.CountryList = new SelectList(CountryList, "CountryId", "CountryName");
+            return View();
+        }
+
+        public JsonResult GetStateList(int CountryId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<State> StateList = db.States.Where(x => x.CountryId == CountryId).ToList();
+            return Json(StateList, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
+    
---
+++ dropdownajax/DropDownAjax/Models/CountryStateModel.cs
@@ -0,0 +1,13 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DropDownAjax.Models
+{
+    public class CountryStateModel
+    {
+        public int CountryId { get; set; }
+        public int StateId { get; set; }
+    }
+}
